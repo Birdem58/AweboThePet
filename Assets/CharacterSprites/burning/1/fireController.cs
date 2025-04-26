60b8fdb5f16276a4e373378e89f7b881553965e0
Aleyna Ak <76571834+Rosenkreig@users.noreply.github.com>
2025-04-26 18:16:49 +0300

    canvas+fire

---
+++ Assets/CharacterSprites/burning/1/fireController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireController : MonoBehaviour
+{
+    private Animator animator;
+    private bool isExtinguished = false;
+    private bool isEnding = false;
+    private float endTimer = 0f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (isEnding)
+        {
+            endTimer += Time.deltaTime;
+            if (endTimer >= 0.4f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (!isExtinguished)
+        {
+            animator.SetTrigger("Extinguish");
+            isExtinguished = true;
+            isEnding = true;
+        }
+    }
+}
